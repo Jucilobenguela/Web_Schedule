0965878c245583f7b3126e0d666f8ca7a7d49424
Jucilobenguela <Jucilobenguela@hotmail.com>
2024-07-25 21:10:30 +0100

    create repository

---
+++ backEnd_schedule/src/main/java/com/benguela/backEnd_schedule/BackEndScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.benguela.backEnd_schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndScheduleApplication.class, args);
+	}
+
+}
---
+++ backEnd_schedule/src/test/java/com/benguela/backEnd_schedule/BackEndScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benguela.backEnd_schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
